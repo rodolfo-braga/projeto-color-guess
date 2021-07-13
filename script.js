ddf67a829257829a6100152bf1e20bf169a81f2c
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-07-13 00:57:55 -0300

    Requisitos 3 e 4

--- script.js
+++ script.js
@@ -14,3 +14,17 @@ let rgbColor = document.createElement('p');
 rgbColor.id = 'rgb-color';
 rgbColor.innerHTML = generateRandomColor();
 gameContainer.appendChild(rgbColor);
+
+// Cria as opções de cores a serem adivinhadas
+const colorsContainer = document.createElement('div');
+colorsContainer.id = 'colors-container';
+gameContainer.appendChild(colorsContainer);
+
+const numberOfColors = 6;
+
+for (let index = 0; index < numberOfColors; index += 1) {
+  const colors = document.createElement('div');
+  colors.className = 'ball';
+  colorsContainer.appendChild(colors);
+  colors.style.backgroundColor = `rgb${generateRandomColor()}`;
+}
